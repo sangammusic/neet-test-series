3c16573f773bf2f3eb87e42c4a9e3eb812ad9fe1
sangammusic <missoneet1108@gmail.com>
2026-09-14 00:28:58 +0530

    Update __init__.py

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -11,3 +11,5 @@ from app.admin import decorators  # noqa: E402,F401
 from app.admin import routes  # noqa: E402,F401
 from app.admin import subject_routes  # noqa: E402,F401
 from app.admin import chapter_routes  # noqa: E402,F401
+from app.admin import question_routes  # noqa: E402,F401
+from app.admin import test_routes  # noqa: E402,F401
